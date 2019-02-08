88d38bc234206b2f74e30472908b53a7e85431c8
danielclas <47436613+danielclas@users.noreply.github.com>
2019-02-07 21:55:55 -0300

    Primer clase
    Ejercicios 1 al 4

---
+++ jsEntradaSalida-3.js
@@ -0,0 +1,13 @@
+/*Debemos lograr tomar un dato por 'ID'
+y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
+function mostrar()
+{
+
+	elNombre.value="juan";
+	var nombre;
+	nombre = elNombre.value;
+	alert (nombre);
+
+}
+
+
