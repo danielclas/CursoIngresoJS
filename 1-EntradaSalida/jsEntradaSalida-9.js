f169d79d88b57748684f400367918dbf722daecd
alumno <alumno>
2019-02-08 22:05:23 -0300

    Ejercicio 8 realizado

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -5,5 +5,18 @@ en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
 
+	var salario; //creo las variables
+	var aumento;
+
+	salario=sueldo.value; //asigno el valor a las variables a traves del ID
+
+	salario=parseInt(salario); //transformo el valor a entero
+
+	aumento=salario*10/100+salario; //defino la variable como aumento + sueldo
+
+	resultado.value=aumento; //muestro la variable aumento
+
+
+
 	
 }
