ef4c87351c089930ec752979c4732f2ec257871c
alumno <alumno@utnfra.com.ar>
2019-02-14 20:07:48 -0300

    ejercicio 4 realizado

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,31 @@
 function mostrar()
 {
+	var amigos;
+	var cuenta;
+	var propina;
+	var iva;
+	var coniva;
+	var total;
+	var cadauno;
+
+	amigos=prompt("Cantidad de amigos:");
+	cuenta=prompt("La cuenta es de:");
+
+	amigos=parseInt(amigos);
+	cuenta=parseInt(cuenta);
+
+	propina=cuenta*10/100;
+
+	iva=cuenta*21/100;
+
+	coniva=cuenta+iva;
+
+	total=coniva+propina;
+
+	cadauno=total/amigos;
+
+	alert("El total incluyendo propina e iva es: $"+total+". Divido "+amigos+" amigos, cada uno debera pagar $"+cadauno);
+
+
 
 }
