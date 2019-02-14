64a676447a60e70511d01cf106058bb55e505102
alumno <alumno@utnfra.com.ar>
2019-02-14 19:26:03 -0300

    ejercicio 1 resuelto

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,29 @@
 
 function mostrar()
 {
+	var mascota1;
+
+	mascota1=prompt("Mascota 1=");
+
+	var mascota2;
+
+	mascota2=prompt("Mascota 2=");
+
+	var peso1;
+
+	peso1=prompt("Peso de mascota 1");
+
+	var peso2;
+
+	peso2=prompt("Peso de mascota 2");
+
+	peso1=parseInt(peso1);
+	peso2=parseInt(peso2);
+
+	var suma;
+
+	suma=peso1+peso2;
+
+	alert("Tenes dos mascotas: "+mascota1+" y "+mascota2+", y la suma de los pesos es: "+suma);
 
 }
