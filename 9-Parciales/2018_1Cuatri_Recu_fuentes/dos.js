e003bdbd1ba720eb9fd035a955914b98b362ae33
alumno <alumno@utnfra.com.ar>
2019-02-14 19:51:56 -0300

    Ejercicio 2 realizado

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
@@ -1,4 +1,24 @@
 function mostrar()
 {
-  
+  var compra;
+  var descuento;
+  var condescuento;
+  var iva;
+  var coniva;
+
+  compra=prompt("Ingrese el valor de su compra");
+  compra=parseInt(compra);
+
+  descuento=compra*10/100;
+
+  condescuento=compra-descuento;
+
+  iva=condescuento*21/100;
+
+  coniva=condescuento+iva;
+
+  alert("Tu compra es de "+compra+", tenes un descuento del 10% queda en "+condescuento+", mas el iva es "+coniva);
+
+
+
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -2,19 +2,13 @@
 function mostrar()
 {
 	var mascota1;
-
-	mascota1=prompt("Mascota 1=");
-
 	var mascota2;
-
-	mascota2=prompt("Mascota 2=");
-
 	var peso1;
-
-	peso1=prompt("Peso de mascota 1");
-
 	var peso2;
 
+	mascota1=prompt("Mascota 1=");
+	mascota2=prompt("Mascota 2=");
+	peso1=prompt("Peso de mascota 1");
 	peso2=prompt("Peso de mascota 2");
 
 	peso1=parseInt(peso1);
@@ -24,6 +18,6 @@ function mostrar()
 
 	suma=peso1+peso2;
 
-	alert("Tenes dos mascotas: "+mascota1+" y "+mascota2+", que suman "+peso1+" y "+peso2+" kilos, y la suma de los pesos es: "+suma);
+	alert("Tenes dos mascotas: "+mascota1+" y "+mascota2+", que pesan "+peso1+" y "+peso2+" kilos, y la suma de los pesos es: "+suma);
 
 }
