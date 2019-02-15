63ffb5e562972730c3227f9af4e944f1ccc818df
alumno <alumno>
2019-02-15 19:02:26 -0300

    Ejercicio 10 hecho en clase

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,31 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
+	var numero;
+	var mensaje;
+
+	numero=Math.floor((Math.random() * 10) + 1);
+
+	if(numero>8 && numero<11)
+	{
+			mensaje="Excelente";
+	}
+	else
+	{	if(numero>4)
+		{
+					mensaje="Aprobó";
+		}
+		else
+		{
+				mensaje="Vamos, la próxima se puede";
+		}
+
+
+		}
+
+	alert("Su nota es "+numero+", "+mensaje);
+
+
 	
 
 }//FIN DE LA FUNCIÓN
