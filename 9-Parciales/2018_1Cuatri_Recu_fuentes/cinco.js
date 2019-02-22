7bfa175a14e8f795bda37849dd8f7c300f36eb1b
alumno <alumno>
2019-02-21 21:18:35 -0300

    Tps hechos en clase

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
@@ -1,6 +1,71 @@
 function mostrar()
 {
-	var dia;
+	var preciohab;
+	var paquete;
+	var tipodepago;
+	var porcentaje;
+	var preciofinal;
+
+	preciohab=prompt("Ingrese precio de habitacion");
+	paquete=prompt("Ingrese un paquete");
+	tipodepago=prompt("Ingrese tipo de pago");
+
+	preciohab=parseInt(preciohab);
+
+	switch(tipodepago)
+	{
+		case "visa":
+		case "mercadoPago":
+			porcentaje=0.90;
+				break;
+		case "paypal":
+		
+			switch(paquete)
+			{
+				case "todoIncluido":
+					porcentaje=0.75;
+						break;
+				default:
+					porcentaje=0.85;
+						break;
+			}
+				break;
+			
+		
+		case "efectivo":
+			
+			switch (paquete)
+			{
+				case "soloDesayunos":
+					porcentaje=0.70;
+						break;
+				case "todoIncluido":
+					porcentaje=0.65;
+						break;
+				default:
+					porcentaje=0.80
+						break;
+			}
+				break;
+			}
+		
+		default:
+			porcentaje=0.95;
+				break;
+				
+	}
+	
+	preciofinal=preciohab*porcentaje;
+
+	alert("El precio es "+preciofinal);
+
+
+
+}
+
+/*
+
+var dia;
 	var mensaje;
 
 	dia=prompt("Ingrese un dia de la semana");
@@ -24,4 +89,5 @@ function mostrar()
 	}
 
 	alert(mensaje);
-}
+
+	*/
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,5 +1,99 @@
 function mostrar()
 {
+	var valor1;
+	var valor2;
+	var valor3;
+	var valor4;
+	var valorfinal;
+	var suma;
+	//var descuento;
+	//var aumento;
+	//var condescuento;
+	//var conaumento;
+	var mensaje;
+	var masalto;
+	var porcentaje;
+
+	valor1=prompt("Ingrese el primer numero");
+	valor2=prompt("Ingrese el segundo numero");
+	valor3=prompt("Ingrese el tercer numero");
+	valor4=prompt("Ingrese el cuarto numero");
+
+	valor1=parseInt(valor1);
+	valor2=parseInt(valor2);
+	valor3=parseInt(valor3);
+	valor4=parseInt(valor4);
+
+	suma=valor1+valor2+valor3+valor4;
+
+	if(valor1>valor2 && valor1>valor3 && valor1>valor4)
+	{
+		masalto=valor1;
+	}
+	else
+	{
+		if(valor2>valor3 && valor2>valor4)
+		{
+			masalto=valor2;
+		}
+		else
+		{
+			if(valor3>valor4)
+			{
+				masalto=valor3;
+			}
+			else
+			{
+				masalto=valor4;
+			}
+		}
+	}
+
+
+	if(suma>100)
+	{
+		porcentaje=0.90;
+		//descuento=suma*10/100;
+		//condescuento=suma-descuento;
+		//mensaje="Precio final: "+condescuento;
+	}
+	else
+	{
+		if(suma>50 && suma<101)
+		{
+			porcentaje=0.95;
+			//descuento=suma*5/100;
+			//condescuento=suma-descuento;
+			//mensaje="Precio final: "+condescuento;
+		}
+		else
+		{
+			porcentaje=1.15;
+			//aumento=suma*15/100;
+			//conaumento=suma+aumento;
+			//mensaje="Precio final: "+conaumento;
+		}
+	}
+
+	//condescuento=suma-(suma*descuento);
+	//conaumento=suma+(suma*aumento);
+
+	valorfinal=suma*porcentaje;
+
+	//condescuento=suma-descuento;
+	//conaumento=suma+aumento;
+
+	alert("El importe mas alto es "+masalto+". Valor final: "+valorfinal);
+
+	
+
+
+
+
+
+}
+
+/*
 	var amigos;
 	var cuenta;
 	var propina;
@@ -26,6 +120,4 @@ function mostrar()
 
 	alert("El total incluyendo propina e iva es: $"+total+". Divido "+amigos+" amigos, cada uno debera pagar $"+cadauno);
 
-
-
-}
+	*/
