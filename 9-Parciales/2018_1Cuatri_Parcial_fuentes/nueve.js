4d3654f788d40c55080b8273484c4f130e22c017
danielclas <danielclas@outlook.es>
2019-03-02 20:58:59 -0300

    Practica en casa

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,4 +1,99 @@
 function mostrar()
 {
+	var marca;
+	var peso;
+	var temp;
+	var respuesta;
+	var contadorPares;
+	var pesoMax;
+	var pesoMaxMarca;
+	var pesoMin;
+	var contador;
+	var contadorBajoCero;
+	var acumuladorPeso;
+	var promedioPeso;
+
+	respuesta="si";
+	contador=0;
+	contadorPares=0;
+	contadorBajoCero=0;
+	acumuladorPeso=0;
+
+	while(respuesta=="si")
+	{
+		marca=prompt("Ingrese la marca");
+
+		peso=prompt("Ingrese el peso");
+		peso=parseInt(peso);
+
+		while(peso<1 || peso>100)
+		{    
+			peso=prompt("Ingrese el peso");
+			peso=parseInt(peso);
+		}
+
+		acumuladorPeso=acumuladorPeso+peso;
+
+		temp=prompt("Ingrese la temperatura");
+		temp=parseInt(temp);
+
+		while(temp<-30 || temp>30)
+		{
+			temp=prompt("Ingrese la temperatura");
+			temp=parseInt(temp);
+		}
+
+		if(temp%2==0)
+		{
+			contadorPares++;
+		}
+
+		if(contador==0)
+		{
+			pesoMin=peso;
+			pesoMax=peso;
+			pesoMaxMarca=marca;
+		}
+		else
+		{
+			if(peso<pesoMin)
+			{
+				pesoMin=peso;
+			}
+			else(peso>pesoMax)
+			{
+				pesoMax=peso;
+				pesoMaxMarca=marca;				
+			}
+		}
+
+		if(temp<0)
+		{
+			contadorBajoCero++;
+		}
+
+		contador++;
+		respuesta=prompt("Ingrese si para ingresar otro dato");
+
+	}
+
+	promedioPeso=acumuladorPeso/contador;
+
+	document.write("Cantidad de temp pares: "+contadorPares+"<br>"+
+		"Marca del producto mas pesado: "+pesoMaxMarca+"<br>"+
+		"Productos que se conservan a menos de 0 grados: "+contadorBajoCero+"<br>"+
+		"Promedio del peso de todos los productos: "+promedioPeso+"<br>"+
+		"Peso min y max: "+pesoMin+", "+pesoMax);
+
+
+
+
+
+
+
+
+
+
+
 
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,4 +1,112 @@
 function mostrar()
 {
 
+	var letra;
+	var numero;
+	var contador;
+	var contadorPares;
+	var contadorImpares;
+	var contadorCeros;
+	var acumuladorPositivos;
+	var contadorPositivos;
+	var promedioPositivos;
+	var sumaNegativos;
+	var numMax;
+	var numMin;
+	var numMaxLetra;
+	var numMinLetra;
+	var respuesta;
+
+	respuesta="si";
+
+	contadorPares=0;
+	contadorImpares=0;
+	contadorCeros=0;
+	contadorPositivos=0;
+	acumuladorPositivos=0;
+	sumaNegativos=0;
+	contador=0;
+
+	while(respuesta=="si")
+	{
+		letra=prompt("Ingrese una letra");
+
+		numero=prompt("Ingrese un numero");
+		numero=parseInt(numero);
+
+		while(numero<-100 || numero>100)
+		{
+			numero=prompt("Ingrese un numero valido");
+			numero=parseInt(numero);
+		}
+
+		if(numero%2==0)
+		{
+			contadorPares++;
+		}
+		else
+		{
+			contadorImpares++;
+		}
+
+		if(numero==0)
+		{
+			contadorCeros++;
+		}
+		else
+		{ 
+			if(numero>0)
+			{
+				acumuladorPositivos=acumuladorPositivos+numero;
+				contadorPositivos++;
+			}
+			else
+			{
+				if(numero<0)
+				{
+					sumaNegativos=sumaNegativos+numero;
+				}	
+			}
+		}
+
+		if(contador==0)
+		{
+			numMin=numero;
+			numMax=numero;
+			numMinLetra=letra;
+			numMaxLetra=letra;
+		}
+		else
+		{
+			if(numero<numMin)
+			{
+				numMin=numero;
+				numMinLetra=letra;
+			}
+			else
+			{
+				if(numero>numMax)
+				{
+					numMax=numero;
+					numMaxLetra=letra;
+				}
+			}
+		}
+
+	contador++;
+	respuesta=prompt("Desea ingresar otro dato?");
+
+	}
+
+	promedioPositivos=acumuladorPositivos/contadorPositivos;
+
+
+	document.write("Cantidad de pares: "+contadorPares+"<br>"+
+		"Cantidad de impares: "+contadorImpares+"<br>"+
+		"Cantidad de ceros: "+contadorCeros+"<br>"+
+		"Promedo de positivos: "+promedioPositivos+"<br>"+
+		"Suma de negativos: "+sumaNegativos+"<br>"+
+		"Numero y letra del minimo: "+numMin+numMinLetra+"<br>"+
+		"Numero y letra del maximo: "+numMax+numMaxLetra+"<br>");
+
 }
